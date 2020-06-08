01a7eb14422620ea8d02db555b483808c15efe1f
Mark Edward Ratio <markedwardratio@gmail.com>
2020-06-08 10:57:20 +0800

    Need auth before post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
-  
+  before_action :auth, only: [:index]
+
   def index
   end
+
+  private
+
+    def auth
+      unless user_signed_in?
+        redirect_to new_user_session_path
+      end
+    end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should redirect index when not logged in' do
+    get root_path
+    assert_redirected_to new_user_session_path
+  end
 end
