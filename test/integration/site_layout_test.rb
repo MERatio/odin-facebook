362c5b46a8b860eb9d8851e92b07beb3d48b2475
Mark Edward Ratio <markedwardratio@gmail.com>
2020-06-08 12:21:57 +0800

    Add layout header

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:john)
+  end
+
+  test 'pages for non-signed-in-users' do
+    get root_path
+    assert_select 'a[href=?]', root_path,
+      text: 'Odin Facebook', count: 0
+    assert_select 'a[href=?]', '#', count: 0
+    assert_select 'a[href=?]', root_path,
+      text: 'Home', count: 0
+    assert_select 'a[href=?]', destroy_user_session_path,
+      text: 'Sign out', count: 0
+    assert_redirected_to new_user_session_path
+    follow_redirect!
+    assert_select 'form#new_user[action=?]', user_session_path
+  end
+
+  test 'pages for signed-in-users' do
+    sign_in(@user)
+    get root_path
+    assert_template 'posts/index'
+    assert_select 'a[href=?]', root_path,
+      text: 'Odin Facebook', count: 1
+    assert_select 'a[href=?]', '#', count: 1
+    assert_select 'a[href=?]', root_path,
+      text: 'Home', count: 1
+    assert_select 'a[href=?]', destroy_user_session_path,
+      text: 'Sign out', count: 1
+  end
+end
