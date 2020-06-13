1913d4417b0ebcfe995fae7157847dfce45df1ed
Mark Edward Ratio <markedwardratio@gmail.com>
2020-06-13 13:01:04 +0800

    Add gravatar_for helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,10 @@ module UsersHelper
   def current_user?(user)
     current_user == user
   end
+
+  def gravatar_for(user, size: 180)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.full_name)
+  end
 end
