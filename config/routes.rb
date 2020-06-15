0edb4167d908782210208f648a4281cbfd9f71e3
Mark Edward Ratio <markedwardratio@gmail.com>
2020-06-15 10:42:16 +0800

    Remove password in user edit for FB user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index' 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { 
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks', 
+  }
   resources  :users, only: [:index, :show] do
     member do
       get :friends
