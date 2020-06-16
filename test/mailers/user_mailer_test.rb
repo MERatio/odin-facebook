e4c14df244987b16a836e1cd0761c5da46bb2925
Mark Edward Ratio <markedwardratio@gmail.com>
2020-06-16 11:14:21 +0800

    Change from email

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,11 +4,11 @@ class UserMailerTest < ActionMailer::TestCase
   test 'welcome email' do
     user = users(:john)
     mail = UserMailer.with(user: user).welcome_email
-    assert_equal ['from@example.com'],          mail.from
-    assert_equal 'Welcome to Odin Facebook',    mail.subject
-    assert_equal [user.email],                  mail.to
-    assert_match user.first_name,               mail.body.encoded
+    assert_equal [ENV['FROM_EMAIL']], mail.from
+    assert_equal 'Welcome to Odin Facebook',          mail.subject
+    assert_equal [user.email],                        mail.to
+    assert_match user.first_name,                     mail.body.encoded
     sign_up_url = 'https://odin-facebook-54888.herokuapp.com/users/sign_in'
-    assert_match sign_up_url,                   mail.body.encoded
+    assert_match sign_up_url,                         mail.body.encoded
   end
 end
